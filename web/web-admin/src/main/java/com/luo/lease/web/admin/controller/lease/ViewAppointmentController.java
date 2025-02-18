ce844ab4fce1344d19b9cacbc76dff9f6045c901
Luo。 <2063560283@qq.com>
2025-02-18 15:10:47 +0800

    租赁管理 feat(admin): 实现预约看房分页查询功能
    - 在 ViewAppointmentController 中添加分页查询接口
    - 在 ViewAppointmentService 接口中定义分页查询方法
    - 实现 ViewAppointmentServiceImpl 中的分页查询逻辑
    - 在 ViewAppointmentMapper 接口中添加分页查询 SQL
    - 优化 ViewAppointment 实体类，添加 JsonFormat 注解
    - 更新 XML 映射文件，添加分页查询的 SQL语句
    - 在 application.yml 中配置 Jackson 日期格式和时区

--- web/web-admin/src/main/java/com/luo/lease/web/admin/controller/lease/ViewAppointmentController.java
+++ web/web-admin/src/main/java/com/luo/lease/web/admin/controller/lease/ViewAppointmentController.java
@@ -1,14 +1,21 @@
 package com.luo.lease.web.admin.controller.lease;
 
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.luo.lease.common.result.Result;
+import com.luo.lease.model.entity.ViewAppointment;
 import com.luo.lease.model.enums.AppointmentStatus;
+import com.luo.lease.web.admin.service.ViewAppointmentService;
 import com.luo.lease.web.admin.vo.appointment.AppointmentQueryVo;
 import com.luo.lease.web.admin.vo.appointment.AppointmentVo;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Tag(name = "预约看房管理")
@@ -16,15 +23,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ViewAppointmentController {
 
+    @Autowired
+    private ViewAppointmentService viewAppointmentService;
+
     @Operation(summary = "分页查询预约信息")
     @GetMapping("page")
     public Result<IPage<AppointmentVo>> page(@RequestParam long current, @RequestParam long size, AppointmentQueryVo queryVo) {
-        return Result.ok();
+        IPage<AppointmentVo> page = new Page<>(current, size);
+        IPage<AppointmentVo> list = viewAppointmentService.pageAppointmentByQuery(page,queryVo);
+        return Result.ok(list);
     }
 
     @Operation(summary = "根据id更新预约状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam AppointmentStatus status) {
+        LambdaUpdateWrapper<ViewAppointment> viewAppointmentLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+        viewAppointmentLambdaUpdateWrapper.eq(ViewAppointment::getId,id);
+        viewAppointmentLambdaUpdateWrapper.set(ViewAppointment::getAppointmentStatus,status);
+        viewAppointmentService.update(viewAppointmentLambdaUpdateWrapper);
         return Result.ok();
     }
 
--- web/web-admin/src/main/java/com/luo/lease/web/admin/service/impl/ViewAppointmentServiceImpl.java
+++ web/web-admin/src/main/java/com/luo/lease/web/admin/service/impl/ViewAppointmentServiceImpl.java
@@ -1,9 +1,13 @@
 package com.luo.lease.web.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.luo.lease.model.entity.ViewAppointment;
 import com.luo.lease.web.admin.mapper.ViewAppointmentMapper;
 import com.luo.lease.web.admin.service.ViewAppointmentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.luo.lease.web.admin.vo.appointment.AppointmentQueryVo;
+import com.luo.lease.web.admin.vo.appointment.AppointmentVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +19,13 @@ import org.springframework.stereotype.Service;
 public class ViewAppointmentServiceImpl extends ServiceImpl<ViewAppointmentMapper, ViewAppointment>
         implements ViewAppointmentService {
 
+    @Autowired
+    private ViewAppointmentMapper viewAppointmentMapper;
+
+    @Override
+    public IPage<AppointmentVo> pageAppointmentByQuery(IPage<AppointmentVo> page, AppointmentQueryVo queryVo) {
+        return viewAppointmentMapper.pageAppointmentByQuery(page,queryVo);
+    }
 }
 
 
